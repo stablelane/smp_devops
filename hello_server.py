d36be8981f8fc8e45e447a40b7fe833ecb0acf1f
stablelane <aaanshad09@gmail.com>
2025-07-01 11:55:33 +0530

    change hello world

--- hello_server.py
+++ hello_server.py
@@ -6,7 +6,7 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'hello world')
+        self.wfile.write(b'Hello World')
 
 def run(server_class=HTTPServer, handler_class=HelloHandler, port=8000):
     server_address = ('', port)
